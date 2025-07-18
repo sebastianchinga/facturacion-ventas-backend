055f3adcc61dd3a237f7380794c06d8d5ea3b1b4
Sebastian <jchingapalacios05@gmail.com>
2025-07-18 17:40:57 -0500

    Arreglar problemas de CORS

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import usuarioRoutes from './routes/usuarioRoutes.js';
 import db from './config/db.js';
@@ -9,6 +10,20 @@ dotenv.config();
 db.authenticate().then(() => console.log('Base de datos conectada')).catch(e => console.log(e))
 
 app.use(express.json());
+
+const dominiosPermitidos = [process.env.URL_FRONTEND];
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (dominiosPermitidos.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('No permitido por CORS'))
+        }
+    }
+
+}
+
+app.use(cors(corsOptions));
 
 
 app.use('/api/usuarios', usuarioRoutes)
